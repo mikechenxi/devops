807ff2c906c67f38f88da23e6cc22097e3981663
mikechenxi <xiaoxiaoaxi@msn.cn>
2019-11-18 17:17:09 +0800

    Create java_ldap.java

---
+++ java_ldap.java
@@ -0,0 +1,45 @@
+import java.util.Hashtable;
+
+import javax.naming.AuthenticationException;
+import javax.naming.Context;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+public class Ldap_Login {
+    public static void main(String[] args) {
+        String userName = "username";
+        String password = "password";
+        String host = "xxx.xxx.xxx.xxx";
+        String domain = "@xxx.xxx";
+        String port = "389"; 
+        String url = new String("ldap://" + host + ":" + port);
+        String user = userName.indexOf(domain) > 0 ? userName : userName + domain;
+        Hashtable env = new Hashtable();
+        DirContext ctx = null;
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.SECURITY_PRINCIPAL, user);
+        env.put(Context.SECURITY_CREDENTIALS, password);
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, url);
+        try {
+            ctx = new InitialDirContext(env);
+            System.out.println("success!");
+        } catch (AuthenticationException e) {
+            System.out.println("failed!");
+            e.printStackTrace();
+        } finally{
+            if(null!=ctx){
+                try {
+                    ctx.close();
+                    ctx=null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+/*
+bug: it works when password is empty
+*/
