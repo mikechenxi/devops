b6a890c9488a643414f1567c0a43f5a4694763b9
mikechenxi <xiaoxiaoaxi@msn.cn>
2019-11-21 14:37:54 +0800

    Update python_ldap.py

--- python_ldap.py
+++ python_ldap.py
@@ -4,22 +4,25 @@
 import ldap
 
 def ldap_auth(username, password):
-    ldap_host = 'xxx.xxx.xxx.xxx'
-    ldap_port = '389'
-    ldap_domain = '@xxx.com'
-    username = username if username.find(domain) > 0 else username + ldap_domain
-    conn = ldap.initialize('ldap://' + ldap_host + ':' + ldap_port)
-    try:
-        conn.simple_bind_s(username, password)
-        return True
-    except Exception as e:
+    if len(username) > 0 and len(password) > 0:
+        ldap_host = 'xxx.xxx.xxx.xxx'
+        ldap_port = '389'
+        ldap_domain = '@xxx.com'
+        username = username if username.find(domain) > 0 else username + ldap_domain
+        conn = ldap.initialize('ldap://' + ldap_host + ':' + ldap_port)
+        try:
+            conn.simple_bind_s(username, password)
+            return True
+        except Exception as e:
+            return False
+    else:
         return False
-
 
 
 '''
 - linux needs to install openldap-devel(yum install openldap-devel) 
   before install python-ldap(cd python-ldap-3.2.0 && python setup.py install)
 - win just use pip install xxxxx.whl(from https://www.lfd.uci.edu/~gohlke/pythonlibs/#python-ldap)
+- it works when password is empty
 '''
     
