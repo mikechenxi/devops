06eba86b6a8e086b8742291bdc965c8ddaf08e89
Mike Chen <xiaoxiaoaxi@msn.cn>
2021-01-05 17:10:07 +0800

    Update python_db.py

--- python_db.py
+++ python_db.py
@@ -17,10 +17,12 @@ def query_mysql(sql, param = None):
         conn = pymysql.connect(host = db_server, port = db_port, user = db_user_name, passwd = db_password, db = db_name)
         cursor = conn.cursor()
         cursor.execute(sql, param)
-        data = cursor.fetchall()
+        result = cursor.fetchall()
+        fields = cursor.description
         cursor.close()
         conn.close()
-        return data
+        res = format_data(result, fields)
+        return res
     except Exception as e:
         print(e)
 
@@ -55,10 +57,12 @@ def query_mssql(sql, param = None):
         conn = pymssql.connect(host = db_server, port = db_port, user = db_user_name, password = db_password, database = db_name)
         cursor = conn.cursor()
         cursor.execute(sql, param)
-        data = cursor.fetchall()
+        result = cursor.fetchall()
+        fields = cursor.description
         cursor.close()
         conn.close()
-        return data
+        res = format_data(result, fields)
+        return res
     except Exception as e:
         print(e)
 
@@ -81,3 +85,17 @@ def execute_mssql(sql, param = None):
     except Exception as e:
         print(e)
 
+
+def format_data(result, fields):
+    # 字段数组 ['id', 'name', 'password']
+    field = []
+    for i in fields:
+        field.append(i[0])
+    # 返回的数组集合 形式[{'id': 1, 'name': 'admin', 'password': '123456'}]
+    res = []
+    for iter in result:
+        line_data = {}
+        for index in range(0, len(field)):
+            line_data[field[index]] = iter[index]
+        res.append(line_data)
+    return res
