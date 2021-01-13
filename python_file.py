4861d3c7966f01d62fdb37104feac2f715022b45
Mike Chen <xiaoxiaoaxi@msn.cn>
2021-01-13 18:15:04 +0800

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -1,6 +1,8 @@
-import urllib, urllib2, zipfile, os
+import urllib, urllib2, zipfile, os, sys, xlsxwriter
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def upload_file(url, file_full_name):
@@ -25,4 +27,24 @@ def zip_directory(directory_path, file_full_name):
     zip.close()
 
 
+# data [{'':'', '':''}, {'':'', '':''}]
+def write_to_excel(data, file_full_name):
+    workbook = xlsxwriter.Workbook(file_full_name)
+    worksheet = workbook.add_worksheet()
+    style_title = workbook.add_format({'bold': True, 'align': 'center', 'font_size': 10})
+    style_content = workbook.add_format({'font_size': 9})
+    row = col = 0
+    for line in data:
+        if row == 0:
+            for key in line.keys():
+                worksheet.write(row, col, key, style_title)
+                col += 1
+        row += 1
+        col = 0
+        for key in line.keys():
+            worksheet.write(row, col, str(line[key]) if line[key] is not None else '', style_content)
+            col += 1
+    workbook.close()
+
+    
 # 上传函数参考: https://my.oschina.net/whp/blog/127909
