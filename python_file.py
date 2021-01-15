58344e0f6e8602c332dd1d62d43085730f5c0804
Mike Chen <xiaoxiaoaxi@msn.cn>
2021-01-15 17:48:23 +0800

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -26,10 +26,12 @@ def zip_directory(directory_path, file_full_name):
     zip.close()
 
 def read_from_excel(file_full_name):
-    workbook = openpyxl.load_workbook(file_full_name)
+    workbook = openpyxl.load_workbook(file_full_name, read_only = True)
     worksheet = workbook[workbook.sheetnames[0]]  # 默认读取第一个sheet
-    titles = ret = []
-    row_number = col_number = 0
+    titles = []
+    ret = []
+    row_number = 0
+    col_number = 0
     for row in worksheet.rows:
         js = {}
         for cell in row:
@@ -44,46 +46,81 @@ def read_from_excel(file_full_name):
         col_number = 0
     return ret
 
+# 写入excel时，xlsxwriter比openpyxl快很多，且生成的excel文档要小10%以上
+# 使用openpyxl写入excel时，加上write_only=True参数对性能影响很有限
 def write_to_excel(data, file_full_name):
-    workbook = openpyxl.Workbook()  # 默认有一个名为'Sheet'的sheet
-    workbook.create_sheet()  # 创建 'Sheet1'
-    workbook.remove(workbook.active)  # 删除 'Sheet'  这两行可以不要，只是强迫症要设置sheet名为'Sheet1'
-    worksheet = workbook.active
-    alignment_title = Alignment(horizontal = 'center')
-    font_title = Font(bold = True, size = 10)
-    font_content = Font(size = 9)
-    row_number = col_number = 1
-    for row in data:
-        if row_number == 1:
-            for key in row.keys():
-                worksheet.cell(row_number, col_number, str(key) if key is not None else '')
-                worksheet.cell(row_number, col_number).font = font_title
-                worksheet.cell(row_number, col_number).alignment = alignment_title
-                col_number += 1
-        row_number += 1
-        col_number = 1
-        for key in row.keys():
-            worksheet.cell(row_number, col_number, str(row[key]) if row[key] is not None else '')
-            worksheet.cell(row_number, col_number).font = font_content
-            col_number += 1
-    workbook.save(file_full_name)
-
-def write_to_excel2(data, file_full_name):
     workbook = xlsxwriter.Workbook(file_full_name)
-    worksheet = workbook.add_worksheet()  # 默认sheet名为 'Sheet1'
+    worksheet = workbook.add_worksheet() # 创建名为'Sheet1'的sheet
     style_title = workbook.add_format({'bold': True, 'align': 'center', 'font_size': 10})
     style_content = workbook.add_format({'font_size': 9})
-    row_number = col_number = 0
+    row_number = 0
+    col_number = 0
+    keys = data[0].keys()
+    for key in keys:
+        worksheet.write(row_number, col_number, str(key) if key is not None else '', style_title)
+        col_number += 1
     for row in data:
-        if row_number == 0:
-            for key in row.keys():
-                worksheet.write(row_number, col_number, str(key) if key is not None else '', style_title)
-                col_number += 1
         row_number += 1
         col_number = 0
         for key in row.keys():
             worksheet.write(row_number, col_number, str(row[key]) if row[key] is not None else '', style_content)
             col_number += 1
     workbook.close()
+    log_util.log('file_util.write_to_excel', 'filename:%s' % (file_full_name))
+
+def write_to_excel2(data, file_full_name):
+    workbook = openpyxl.Workbook()  # 默认有一个名为'Sheet'的sheet
+    workbook.create_sheet()  # 创建名为'Sheet1'的sheet
+    workbook.remove(workbook[workbook.sheetnames[0]])  # 删除名为'Sheet'的sheet  以上两行可以不要，只是OCD患者一定要sheet从'Sheet1'开始
+    worksheet = workbook.active  # 也可以用 workbook[workbook.sheetnames[0]]
+    alignment_title = Alignment(horizontal = 'center')
+    font_title = Font(bold = True, size = 10)
+    font_content = Font(size = 9)
+    row_number = 1
+    col_number = 1
+    keys = data[0].keys()
+    for key in keys:
+        worksheet.cell(row_number, col_number, str(key) if key is not None else '')
+        worksheet.cell(row_number, col_number).font = font_title
+        worksheet.cell(row_number, col_number).alignment = alignment_title
+        col_number += 1
+    for row in data:
+        row_number += 1
+        col_number = 1
+        for key in keys:
+            worksheet.cell(row_number, col_number, str(row[key]) if row[key] is not None else '')
+            worksheet.cell(row_number, col_number).font = font_content
+            col_number += 1
+    workbook.save(file_full_name)
+    log_util.log('file_util.write_to_excel', 'filename:%s' % (file_full_name))
+
+def write_to_excel3(data, file_full_name):
+    workbook = openpyxl.Workbook(write_only = True)  # 默认无sheet
+    workbook.create_sheet()  # 创建名为'Sheet'的sheet
+    worksheet = workbook.create_sheet()  # 创建名为'Sheet1'的sheet
+    workbook.remove(workbook[workbook.sheetnames[0]])  # 删除名为'Sheet'的sheet  以上两行可以不要，只是OCD患者一定要 sheet从'Sheet1'开始
+    alignment_title = Alignment(horizontal = 'center')
+    font_title = Font(bold = True, size = 10)
+    font_content = Font(size = 9)
+    keys = data[0].keys()
+    row_number = 1
+    li_cell = []
+    for key in keys:
+        cell = WriteOnlyCell(worksheet, str(key) if key is not None else '')
+        cell.font = font_title
+        cell.alignment = alignment_title
+        li_cell.append(cell)
+    worksheet.append(li_cell)
+    for row in data:
+        row_number += 1
+        li_cell = []
+        for key in keys:
+            cell = WriteOnlyCell(worksheet, str(row[key]) if row[key] is not None else '')
+            cell.font = font_content
+            cell.alignment = alignment_title
+            li_cell.append(cell)
+        worksheet.append(li_cell)
+    workbook.save(file_full_name)
+    log_util.log('file_util.write_to_excel', 'filename:%s' % (file_full_name))
 
 # 上传函数参考: https://my.oschina.net/whp/blog/127909
