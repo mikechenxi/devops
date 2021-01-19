b45b3454e3cb7b0c3bd10f6801da44b483b6bdfd
Mike Chen <xiaoxiaoaxi@msn.cn>
2021-01-19 14:06:11 +0800

    Update python_ldap.py

--- python_ldap.py
+++ python_ldap.py
@@ -1,4 +1,3 @@
-# python2
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -27,22 +26,3 @@ def ldap_auth(username, password):
 - win just use pip install xxxxx.whl(from https://www.lfd.uci.edu/~gohlke/pythonlibs/#python-ldap)
 - it also works when password is empty
 '''
-
-
-## python3
-from ldap3 import Server, Connection
-
-def ldap_auth(username, password):
-    if len(username) > 0 and len(password) > 0:
-        ldap_host = 'xxx.xxx.xxx.xxx'
-        ldap_port = 389
-        ldap_user_format = 'cn=%s, ou=xx users, dc=xx, dc=com'
-        try:
-            server = Server(ldap_host, ldap_port, False)
-            conn = Connection(server, ldap_user_format % username, password)
-            ret = conn.bind()
-            return ret
-        except Exception as e:
-            return False
-    else:
-        return False
