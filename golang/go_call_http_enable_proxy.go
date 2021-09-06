fcfc4f177672a3991af49c890498e202d81d5bda
Mike Chen <xiaoxiaoaxi@msn.cn>
2021-09-06 17:21:51 +0800

    Update and rename go_call_http.go to go_call_http_enable_proxy.go

--- golang/go_call_http_enable_proxy.go
+++ golang/go_call_http_enable_proxy.go
@@ -50,27 +50,3 @@ func CallHttp(requestUrl string, requestData map[string]interface{}, method stri
     _ = json.Unmarshal(body, &res)
     return res
 }
-
-func CallHttp2(requestUrl string, requestData map[string]interface{}, method string) interface{}{
-    method = strings.ToUpper(method)
-    var resp *http.Response
-    var err error
-    if method == "GET" {
-        resp, err = http.Get(requestUrl)
-    } else if method == "POST" {
-        bytesData, _ := json.Marshal(requestData)
-        requestBody := strings.NewReader(string(bytesData))
-        resp, err = http.Post(requestUrl, "application/json", requestBody)
-    } else {
-        os.Exit(1)
-    }
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    var res interface{}
-    _ = json.Unmarshal(body, &res)
-    return res
-}
