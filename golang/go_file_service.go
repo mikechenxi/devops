b98f803f24853563644ac902e327e386d79f2020
Mike Chen <xiaoxiaoaxi@msn.cn>
2021-10-21 16:32:39 +0800

    Update go_file_service.go

--- golang/go_file_service.go
+++ golang/go_file_service.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+    "errors"
     "fmt"
     "net"
     "net/http"
     "os"
     "path/filepath"
     "strconv"
-    "strings"
-    "time"
 )
 
 func main(){
     port := GetPort()
     go CheckPort(port)
+    //time.Sleep(time.Millisecond)
     StartHttp(port)
 }
 
 func GetPort() string {
-    var port string
+    port := ""
     is_num := false
     for is_num == false {
         fmt.Print("请输入共享端口号(纯数字, 比如80): ")
@@ -39,7 +39,6 @@ func GetPort() string {
 func CheckPort(port string) {
     fmt.Println("服务(" + port + ")启动中...")
     for {
-        time.Sleep(time.Millisecond)
         resp, err := http.Get("http://localhost:" + port)
         if err != nil {
             continue
@@ -51,40 +50,75 @@ func CheckPort(port string) {
         break
     }
     fmt.Println("服务(" + port + ")已启动...")
-    DisplayRemind(port)
-}
-
-func DisplayRemind(port string) {
-    addrs, err := net.InterfaceAddrs()
-    if err != nil{
+    ip, err := GetActualIP()
+    if err != nil {
         fmt.Println(err)
-        return
-    }
-    for _, value := range addrs{
-        if ipnet, ok := value.(*net.IPNet); ok && !ipnet.IP.IsLoopback(){
-            if ipnet.IP.To4() != nil{
-                ip := ipnet.IP.String()
-                if strings.HasPrefix(ip, "10.204") || strings.HasPrefix(ip, "172.32") || strings.HasPrefix(ip, "192.168.48") {
-                    fmt.Println("请在浏览器打开 http://" + ip + ":" + port + " 来访问共享文件")
-                    fmt.Println("共享目录为本程序所在目录")
-                    fmt.Println("直接关掉本窗口即可停止共享")
-                    break
-                }
-            }
-        }
+    } else {
+        ShowRemind(ip.String(), port)
     }
 }
 
-func StartHttp(port string){
+func StartHttp(port string) {
     p, _ := filepath.Abs(filepath.Dir(os.Args[0]))
     http.Handle("/", http.FileServer(http.Dir(p)))
-    err := http.ListenAndServe(":" + port, nil)
+    err := http.ListenAndServe(":"+port, nil)
     if err != nil {
         fmt.Println(err)
     }
 }
 
-func IsNum(s string) bool {
-    _, err := strconv.ParseFloat(s, 64)
+func ShowRemind(ip, port string) {
+    fmt.Println("请在浏览器打开 http://" + ip + ":" + port + " 来访问共享文件")
+    fmt.Println("共享目录为本程序所在目录")
+    fmt.Println("直接关掉本窗口即可停止共享")
+}
+
+func GetActualIP() (net.IP, error) {
+    ifaces, err := net.Interfaces()
+    if err != nil {
+        return nil, err
+    }
+    for _, iface := range ifaces {
+        if iface.Flags & net.FlagUp == 0 {
+            continue // interface down
+        }
+        if iface.Flags & net.FlagLoopback != 0 {
+            continue // loopback interface
+        }
+        addrs, err := iface.Addrs()
+        if err != nil {
+            return nil, err
+        }
+        for _, addr := range addrs {
+            ip := GetIpFromAddr(addr)
+            if ip == nil {
+                continue
+            }
+            return ip, nil
+        }
+    }
+    return nil, errors.New("connected to the network?")
+}
+
+func GetIpFromAddr(addr net.Addr) net.IP {
+    var ip net.IP
+    switch v := addr.(type) {
+    case *net.IPNet:
+        ip = v.IP
+    case *net.IPAddr:
+        ip = v.IP
+    }
+    if ip == nil || ip.IsLoopback() {
+        return nil
+    }
+    ip = ip.To4()
+    if ip == nil {
+        return nil // not an ipv4 address
+    }
+    return ip
+}
+
+func IsNum(port string) bool {
+    _, err := strconv.ParseFloat(port, 64)
     return err == nil
 }
